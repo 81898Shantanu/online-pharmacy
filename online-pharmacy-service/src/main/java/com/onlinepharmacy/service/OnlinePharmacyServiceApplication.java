428687a528ca8b6e01cb19be0563f805579e7180
81898Shantanu <Shantanu.stark@gmail.com>
2024-08-13 15:17:07 +0530

    Add admin user creation script during app startup

--- online-pharmacy-service/src/main/java/com/onlinepharmacy/service/OnlinePharmacyServiceApplication.java
+++ online-pharmacy-service/src/main/java/com/onlinepharmacy/service/OnlinePharmacyServiceApplication.java
@@ -1,40 +1,34 @@
 package com.onlinepharmacy.service;
 
-import com.onlinepharmacy.service.config.AppConstants;
-import com.onlinepharmacy.service.entities.Role;
-import com.onlinepharmacy.service.repositories.RoleRepo;
+import com.onlinepharmacy.service.services.DbInitializerService;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DbInitializerService.class)
 @SecurityScheme(
         name = "E-Commerce Application",
         scheme = "bearer",
         type = SecuritySchemeType.HTTP,
         in = SecuritySchemeIn.HEADER)
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class OnlinePharmacyServiceApplication implements CommandLineRunner {
+public class OnlinePharmacyServiceApplication {
 
-    private final RoleRepo roleRepo;
+    private final DbInitializerService dbInitializerService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlinePharmacyServiceApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-        Role adminRole = new Role(AppConstants.ADMIN_ID, "ADMIN");
-        Role userRole = new Role(AppConstants.USER_ID, "USER");
-        List<Role> roles = List.of(adminRole, userRole);
-        List<Role> savedRoles = roleRepo.saveAll(roles);
-        savedRoles.forEach(System.out::println);
+    @PostConstruct
+    public void initializeDatabase() {
+        dbInitializerService.run();
     }
 }
